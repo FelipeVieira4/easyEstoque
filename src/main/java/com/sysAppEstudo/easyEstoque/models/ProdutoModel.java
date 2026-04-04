a1aa09885575b13d6d705829c0151df47ec380d7
FelipeVieira4 <felipevieira58238@gmail.com>
2026-04-04 18:31:52 -0300

    feat: removido obrigatoriedade do campo endereço

--- src/main/java/com/sysAppEstudo/easyEstoque/models/ProdutoModel.java
+++ src/main/java/com/sysAppEstudo/easyEstoque/models/ProdutoModel.java
@@ -1,5 +1,22 @@
 package com.sysAppEstudo.easyEstoque.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Table(name = "produto_table")
+@Data()
 public class ProdutoModel {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Descrição do produto não pode ser em branco")
+    private String descricao;
 }
