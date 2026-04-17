f6a0154ae487be44e0a6fe3d63419b69c7fd8d88
FelipeVieira4 <felipevieira58238@gmail.com>
2026-04-17 19:32:18 -0300

    fix: corrigido padrão id  -> id_natureza

--- src/main/java/com/sysAppEstudo/easyEstoque/models/NaturezaModel.java
+++ src/main/java/com/sysAppEstudo/easyEstoque/models/NaturezaModel.java
@@ -2,6 +2,7 @@ package com.sysAppEstudo.easyEstoque.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 /*
 
@@ -11,39 +12,16 @@ import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="natureza_table")
+@Data
 public class NaturezaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long natureza_id;
 
     @NotBlank(message = "Descrição da natureza de estoque Obrigátorio")
     private String descricao;
 
     @Column(nullable = false,columnDefinition = "boolean default true")
     private boolean ativo=true;
-
-    public long getId() {
-        return id;
-    }
-                      
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
 }
