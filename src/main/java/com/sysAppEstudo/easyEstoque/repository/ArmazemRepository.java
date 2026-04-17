20c6d0a284dbc3766ec89b22083fa3d951a9dade
FelipeVieira4 <felipevieira58238@gmail.com>
2026-04-17 19:23:01 -0300

    removed: reopository e controller do armazem

--- src/main/java/com/sysAppEstudo/easyEstoque/repository/ArmazemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sysAppEstudo.easyEstoque.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.sysAppEstudo.easyEstoque.models.ArmazemModel;
-
-@Repository
-public interface  ArmazemRepository extends JpaRepository<ArmazemModel,Long>{}
