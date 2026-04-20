32f1d38f9b2a0a35ba00a18b63b46e50f32f54e0
FelipeVieira4 <felipevieira58238@gmail.com>
2026-04-19 21:33:05 -0300

    fix: atualizado padrão de classes models

--- src/main/java/com/sysAppEstudo/easyEstoque/models/NaturezaModel.java
+++ src/main/java/com/sysAppEstudo/easyEstoque/models/NaturezaModel.java
@@ -1,8 +1,9 @@
 package com.sysAppEstudo.easyEstoque.models;
 
+import java.io.Serializable;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
 /*
 
@@ -11,17 +12,41 @@ import lombok.Data;
 */
 
 @Entity
-@Table(name="natureza_table")
-@Data
-public class NaturezaModel {
+@Table(name="natureza")
+public class NaturezaModel implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long natureza_id;
+    private Long idNatureza;
 
-    @NotBlank(message = "Descrição da natureza de estoque Obrigátorio")
+    @NotBlank(message = "Descrição da Natureza de estoque obrigatório")
     private String descricao;
 
     @Column(nullable = false,columnDefinition = "boolean default true")
     private boolean ativo=true;
+
+    public Long getIdNatureza() {
+        return idNatureza;
+    }
+
+    public void setIdNatureza(Long idNatureza) {
+        this.idNatureza = idNatureza;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+    
 }
--- src/main/java/com/sysAppEstudo/easyEstoque/models/ProdutoModel.java
+++ src/main/java/com/sysAppEstudo/easyEstoque/models/ProdutoModel.java
@@ -1,22 +1,69 @@
 package com.sysAppEstudo.easyEstoque.models;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "produto_table")
-@Data()
-public class ProdutoModel {
+@Table(name = "produtos")
+public class ProdutoModel implements Serializable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idProduto;
 
-    @NotBlank(message = "Descrição do produto não pode ser em branco")
+    @NotBlank(message = "Descrição do Produto não pode ser em branco")
     private String descricao;
+
+    @NotNull(message = "Natureza do Produto é obrigatório")
+    @ManyToOne
+    @JoinColumn(name = "idNatureza")
+    private NaturezaModel natureza;
+
+    @NotNull(message = "Preço Médio campo obrigatório")
+    @Min(value = 0, message = "O preço não pode ser negativo")
+    private BigDecimal precoMedio;
+
+    public Long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public NaturezaModel getNatureza() {
+        return natureza;
+    }
+
+    public void setNatureza(NaturezaModel natureza) {
+        this.natureza = natureza;
+    }
+
+    public BigDecimal getPrecoMedio() {
+        return precoMedio;
+    }
+
+    public void setPrecoMedio(BigDecimal precoMedio) {
+        this.precoMedio = precoMedio;
+    }
+    
 }
