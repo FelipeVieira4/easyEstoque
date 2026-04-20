4d3e035a201fd63a83f38bbff46d1a009b187def
FelipeVieira4 <felipevieira58238@gmail.com>
2026-04-19 21:36:33 -0300

    removed: controller desnecessário

--- src/main/java/com/sysAppEstudo/easyEstoque/controller/FunRestController.java
+++
@@ -1,9 +0,0 @@
-package com.sysAppEstudo.easyEstoque.controller;
-
-import org.springframework.beans.factory.annotation.Value;
-
-public class FunRestController {
-    
-    @Value("${coach.name}")
-    private String coachName;
-}
